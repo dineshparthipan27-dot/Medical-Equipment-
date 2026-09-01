15e9c3eb0972b82111f76ed9cf38c2d76e97cb40
dineshparthipan27-dot <dineshparthipan27@gmail.com>
2026-09-01 16:28:12 +0530

    Medical Equipment

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
- 
-const mobileToggle = document.getElementById('mobile-toggle');
+ const mobileToggle = document.getElementById('mobile-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 const toggleIcon = mobileToggle.querySelector('i');
 
- 
 mobileToggle.addEventListener('click', function () {
-
- 
     if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
- 
+       
         mobileMenu.style.display = 'block';
- 
         toggleIcon.classList.remove('fa-bars');
         toggleIcon.classList.add('fa-xmark');
+        
+       
+        document.body.style.overflow = 'hidden'; 
     } else {
- 
+       
         mobileMenu.style.display = 'none';
- 
         toggleIcon.classList.remove('fa-xmark');
         toggleIcon.classList.add('fa-bars');
+        
+        
+        document.body.style.overflow = ''; 
     }
 });
